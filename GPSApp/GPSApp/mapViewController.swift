9b437e49df1d530b551af685a42d24249f43dd05
Jitin Dodd <j2dodd@uwaterloo.ca>
2016-03-21 23:22:19 -0400

    map interaction and image fetching - initial.

--- GPSApp/GPSApp/mapViewController.swift
+++ GPSApp/GPSApp/mapViewController.swift
@@ -9,31 +9,40 @@
 import UIKit
 import GoogleMaps
 import CoreLocation
+import Alamofire
 
 class MapViewController: UIViewController, CLLocationManagerDelegate {
     
     let locationManager = CLLocationManager()
+    let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: GMSCameraPosition.cameraWithLatitude(43.4784140, longitude: -80.5226360, zoom: 16))
+    let marker = GMSMarker()
+    var userImage = UIImage()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
-            longitude: 151.20, zoom: 6)
-        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        mapView.myLocationEnabled = true
+        self.mapView.myLocationEnabled = true
         self.view = mapView
         
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
+        self.mapView.myLocationEnabled = false
+//        let marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2DMake(43.4784140, -80.5226360)
+//        marker.title = "Sydney"
+//        marker.snippet = "Australia"
+//        marker.map = mapView
+        
         
         self.locationManager.delegate = self
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
         //locationManager.requestAlwaysAuthorization() //for background use
         self.locationManager.requestWhenInUseAuthorization()
         self.locationManager.startUpdatingLocation()
+        
+        
+        Alamofire.request(.GET, "https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/s150/photo.jpg").response() {
+            (_, _, data, _) in
+            self.userImage = UIImage(data: data! )!
+        }
     }
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -41,6 +50,14 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         let parameters : [String: Double] = ["latitude": locValue.latitude, "longitude": locValue.longitude]
         DownloadManager.sharedInstance.postLocation(parameters)
         print("Broadcasting location..")
+        updateMap(locValue)
+    }
+    
+    func updateMap(position: CLLocationCoordinate2D) {
+        mapView.animateToLocation(position)
+        self.marker.position = position
+        self.marker.map = self.mapView
+        self.marker.icon = self.userImage
     }
     
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
